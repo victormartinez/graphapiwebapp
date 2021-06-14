910a32d4bb45f459696087055fdfffe8c1760fc4
Victor Martinez <vcrmartinez@gmail.com>
2021-06-14 09:13:57 -0300

    feat: configure routes/pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import './reset.css';
+import HomePage from './pages/HomePage';
+import NodePage from './pages/NodePage';
+import FriendsPage from './pages/FriendsPage';
+import FriendsOfFriendsPage from './pages/FriendsOfFriendsPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/nodes" component={NodePage} />
+        <Route exact path="/friends" component={FriendsPage} />
+        <Route exact path="/friends-of-friends" component={FriendsOfFriendsPage} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
