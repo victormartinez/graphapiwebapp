f2b429672ccab05645a3ebf8daafd912c14ba17b
Victor Martinez <vcrmartinez@gmail.com>
2021-06-14 15:59:27 -0300

    fix: button style

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  // width: 100%;
-  // height: 2rem;
-  // cursor: pointer;
-  // font-weight: 900;
-  // border-radius: 46px;
-  // margin: 0.5rem 0 0 0;
-  // border: solid 1px black;
+  padding: .375rem .75rem;
+  cursor: pointer;
+  font-weight: 900;
+  margin: 0.5rem 0 0 0;
+  border: none;
 
-  // &:hover {
-  //   background-color: white;
-  // }
+  &.success {
+    background-color: #21df86;
+  }
+  &.success:hover {
+    opacity: 0.7;
+  }
 `;
