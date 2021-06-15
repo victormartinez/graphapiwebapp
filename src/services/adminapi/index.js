4736e65b17d7da2770020b113b429dc5b2d5de9f
Victor Martinez <vcrmartinez@gmail.com>
2021-06-14 21:49:48 -0300

    feat: add create endpoint to graphApi service

--- src/services/adminapi/index.js
+++ src/services/adminapi/index.js
@@ -7,9 +7,22 @@ const graphApi = axios.create({
 
 export async function listNodes() {
   return graphApi.get(`/api/v1/graph/nodes`, {
-    crossdomain: true,
     headers: {
       'Content-Type': 'application/json',
     },
   });
 }
+
+export async function createNode(name, friends) {
+  const data = {
+    'name': name,
+    'friends': friends || []
+  }
+  return graphApi.post(`/api/v1/graph/nodes`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+}
+
